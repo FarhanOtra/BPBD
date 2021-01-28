34c1c135e45a1ffcdb7f53c08b71f09989bc3c90
FarhanOtra <otra.farhan@gmail.com>
2021-01-28 21:24:42 +0700

    First

---
+++ database/migrations/2021_01_18_012342_all_table.php
@@ -0,0 +1,128 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AllTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jenis_barang', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->string('jenis_barang');
+            $table->timestamps();   
+        });
+
+        Schema::create('barang', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->string('id_barang');
+            $table->string('nama_barang');
+            $table->string('deskripsi')->nullable();
+            $table->unsignedBigInteger('jenisbarang_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('jenisbarang_id')->references('id')->on('jenis_barang')->onDelete('set null');
+        });
+        
+        Schema::create('pihak_pertama', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->string('nama');
+            $table->string('jabatan');
+            $table->string('instansi');
+            $table->timestamps();
+        });
+        
+        Schema::create('pihak_kedua', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->string('nama');
+            $table->string('jabatan');
+            $table->string('instansi');
+            $table->timestamps();
+        });
+
+        Schema::create('donatur', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->string('nama');
+            $table->string('jabatan');
+            $table->string('instansi');
+            $table->timestamps();
+        });
+        
+        Schema::create('penerima', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->string('nama');
+            $table->string('jabatan');
+            $table->string('instansi');
+            $table->timestamps();
+        });
+
+        Schema::create('berita_masuk', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->date('tanggal');
+            $table->unsignedBigInteger('donatur_id')->nullable();
+            $table->unsignedBigInteger('penerima_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('donatur_id')->references('id')->on('donatur')->onDelete('set null');
+            $table->foreign('penerima_id')->references('id')->on('penerima')->onDelete('set null');
+        });
+
+        Schema::create('berita_keluar', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->date('tanggal');
+            $table->unsignedBigInteger('pihak_pertama_id')->nullable();
+            $table->unsignedBigInteger('pihak_kedua_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('pihak_pertama_id')->references('id')->on('pihak_pertama')->onDelete('set null');
+            $table->foreign('pihak_kedua_id')->references('id')->on('pihak_kedua')->onDelete('set null');
+        });
+        
+        Schema::create('detail_masuk', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->unsignedBigInteger('berita_masuk_id')->nullable();
+            $table->unsignedBigInteger('barang_id')->nullable();
+            $table->date('exp');
+            $table->integer('jumlah');
+            $table->integer('stock');
+            $table->timestamps();
+
+            $table->foreign('berita_masuk_id')->references('id')->on('berita_masuk')->onDelete('set null');
+            $table->foreign('barang_id')->references('id')->on('barang')->onDelete('set null');;
+        });
+
+        Schema::create('detail_keluar', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->unsignedBigInteger('berita_keluar_id')->nullable();
+            $table->unsignedBigInteger('barang_id')->nullable();
+            $table->integer('jumlah');
+            $table->string('satuan');
+            $table->string('keterangan');
+            $table->timestamps();
+
+            $table->foreign('berita_keluar_id')->references('id')->on('berita_keluar')->onDelete('set null');
+            $table->foreign('barang_id')->references('id')->on('barang')->onDelete('set null');;
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jenis_barang');
+        Schema::dropIfExists('barang');
+        Schema::dropIfExists('pihak_pertama');
+        Schema::dropIfExists('pihak_kedua');
+        Schema::dropIfExists('transaksi');
+        Schema::dropIfExists('detail_transaksi');
+    }
+}
---
+++ resources/views/barang/edit.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Header -->
+    <div class="header bg-gradient-primary pb-7 pt-5 pt-md-7">
+      <div class="container-fluid">
+        <div class="header-body">
+          <div class="row align-items-center">
+            <div class="col-lg-6 col-7">
+              <h6 class="h2 text-white d-inline-block mt-0 mb-0">Barang</h6>
+              <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                  <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+                  <li class="breadcrumb-item"><a href="{{route('barang.index')}}">List Barang</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Edit Barang</li>
+                </ol>
+              </nav>
+            </div>
+            <div class="col-lg-6 col-5 text-right">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+   </div>
+    <!-- Page content -->
+    <div class="container-fluid mt--6">
+        <div class="row">
+        <div class="col">
+                <div class="card bg-secondary shadow">
+                    <div class="card-header bg-white border-0">
+                        <div class="row align-items-center">
+                            <h3 class="col-12 mb-0">{{ __('Edit Barang') }}</h3>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="{{ route('barang.update',[$barang->id]) }}" autocomplete="off">
+                            @csrf
+                            @method('put')
+                            @if (session('status'))
+                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    {{ session('status') }}
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                            @endif
+
+                            <div class="pl-lg-4">
+                                @include('barang._formedit')
+                                <div class="text-left">
+                                    <button type="submit" class="btn btn-success mt-4">{{ __('Edit') }}</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    @include('layouts.footers.auth')
+  </div>
+@endsection
+
+@push('js')
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
