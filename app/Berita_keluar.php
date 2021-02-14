9532f655ffb523cb037ec7473fc0e4d5acc9e044
irfanrr06 <60853219+irfanrr06@users.noreply.github.com>
2021-02-14 18:12:34 +0700

    Initial commit

--- app/Berita_keluar.php
+++ app/Berita_keluar.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Berita_keluar extends Model
 {
     protected $table = 'berita_keluar';
+
+    public function pihak_pertama()
+    {
+        return $this->hasOne(Pihak_pertama::class,'id','pihak_pertama_id');
+    }
+
+    public function pihak_kedua()
+    {
+        return $this->hasOne(Pihak_kedua::class,'id','pihak_kedua_id');
+    }
 }
