115b5b334f0a570979b624b122883842106c9530
irfanrr06 <60853219+irfanrr06@users.noreply.github.com>
2021-02-16 11:32:12 +0700

    Update create.blade.php

--- resources/views/beritamasuk/create.blade.php
+++ resources/views/beritamasuk/create.blade.php
@@ -30,7 +30,7 @@
                 <div class="card bg-secondary shadow">
                     <div class="card-header bg-white border-0">
                         <div class="row align-items-center">
-                            <h3 class="col-12 mb-0">{{ __('Tambah Berita Masuk') }}</h3>
+                            <h3 class="col-12 mb-0">{{ __('Tambah Barang Masuk') }}</h3>
                         </div>
                     </div>
                     <div class="card-body">
